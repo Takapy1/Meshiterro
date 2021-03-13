533aff7d237d844933dcfaeaa4eae5cf48c1b6f1
Takapy1 <takahirog1109mai@gmail.com>
2021-03-13 09:41:11 +0000

    [Add] comment function before create action

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '86a14c5fdba7ef7a63d292573162c317b232fce631b4fe8c1cb5548d11138d458c63d656321e0969f89ec2044895878b32c9357302199345b7b645775d1ad2bb'
